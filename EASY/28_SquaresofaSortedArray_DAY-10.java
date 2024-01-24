357351837cc9e8fedf9a4eb206eeb093b21d2a25
dhirajgadekar <dhirajgadekar629@gmail.com>
2024-01-24 21:33:06 +0530

    Day 10 Platform codes

---
+++ EASY/28_SquaresofaSortedArray_DAY-10.java
@@ -0,0 +1,15 @@
+/*
+ * 28] Squares of a Sorted Array
+ */
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        for(int i = 0; i < nums.length; i++) {
+
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
