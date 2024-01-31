b04e4f4a321edfaf5369f06b2b86790766c832a5
dhirajgadekar <dhirajgadekar629@gmail.com>
2024-01-31 21:58:48 +0530

    Day 17 Medium Platform codes

---
+++ MEDIUM/2_KthSmallestElement_DAY-17.java
@@ -0,0 +1,11 @@
+/* *
+ * 2] Kth smallest element
+ */
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) {
+
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+}
