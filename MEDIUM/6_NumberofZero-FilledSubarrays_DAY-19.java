131ad754a717cf7c2b2e543f0f0e587bf61afda7
dhirajgadekar <dhirajgadekar629@gmail.com>
2024-02-02 21:11:25 +0530

    Day 19 Medium Platform codes

---
+++ MEDIUM/6_NumberofZero-FilledSubarrays_DAY-19.java
@@ -0,0 +1,35 @@
+/* *
+ * 6] Number of Zero-Filled Subarrays
+ */
+
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        
+	int i = 0;
+        long count = 0;
+        long ans = 0;
+        
+        while(i != nums.length){
+
+            if(nums[i] == 0){
+                count++;
+                i++;
+                continue;
+            } else{
+
+                if(count != 0){
+                    ans += (count*(count+1))/2;
+                }
+                count = 0;
+            }
+            i++;
+        }
+        if(nums[nums.length-1] == 0){
+            al.add(count);
+        }
+         if(count != 0){
+            ans += (count*(count+1))/2;
+        }
+        return ans;
+    }
+}
