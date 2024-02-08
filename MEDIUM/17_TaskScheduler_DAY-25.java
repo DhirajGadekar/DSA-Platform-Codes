c25c75e91eff53ebc116cdb5afa543d2de24f3fe
dhirajgadekar <dhirajgadekar629@gmail.com>
2024-02-08 21:26:31 +0530

    Day 25 Medium Platform codes

---
+++ MEDIUM/17_TaskScheduler_DAY-25.java
@@ -0,0 +1,22 @@
+/* *
+ * 17] Task Scheduler
+ */
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] map = new int[26];
+        for(char c : tasks){
+            map[c-'A']++;
+        }
+
+        Arrays.sort(map);
+        int maxVal = map[25] - 1;
+        int idleSlots = maxVal * n;
+
+        for(int i=24; i>=0; i--){
+            idleSlots -= Math.min(map[i], maxVal);
+        }
+
+        return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
+    }
+}
